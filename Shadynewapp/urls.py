e21dfd12b6f92f002ddcfc221749a4efc0342a1f
shady597 <shady4pf@gmail.com>
2025-11-27 12:15:36 +0300

    Initialize Django project with Shadynewapp, configure settings, and add basic structure

---
+++ Shadynewapp/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from . import views 
+
+urlpatterns = [
+    path('', views.runtemplate, name='runtemplate'),
+    path('', views.say_hi, name='say_hi'),
+]
---
+++ Shadynewapp/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+def say_hi(request):
+   return HttpResponse("Hi there! Welcome to Shadynewapp.")
+
+def runtemplate(request):
+       return render(request, 'Shadynewapp/shady.html')
