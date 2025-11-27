c7e32282a6eaf47110d4f2c97d7ded401b9d0e5a
shady597 <shady4pf@gmail.com>
2025-11-27 12:16:24 +0300

    Refactor URL routing and views; remove unused runtemplate function

--- Shadynewapp/urls.py
+++ Shadynewapp/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views 
 
 urlpatterns = [
-    path('', views.runtemplate, name='runtemplate'),
+#    path('', views.runtemplate, name='runtemplate'),
     path('', views.say_hi, name='say_hi'),
 ]
